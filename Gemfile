f10fdfc4dc35d6ea90066b13ed87eab68915b2e0
Nazrul <nazrulku07@gmail.com>
2017-10-21 15:45:14 +0600

    add capistrano rm gem

--- Gemfile
+++ Gemfile
@@ -65,6 +65,7 @@ group :development, :test do
 
   gem 'capistrano', '~> 3.6'
   gem 'capistrano-rails', '~> 1.3'
+  gem 'capistrano-rvm'
 end
 
 group :development do
--- config/deploy.rb
+++ config/deploy.rb
@@ -33,12 +33,12 @@ set :rvm_ruby_version, '2.2.1'
 set :linked_files, %w{config/database.yml config/secrets.yml}
 
 # Default value for linked_dirs is []
-set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/assets public/uploads pdf_files}
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/assets public/uploads public/spree pdf_files}
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
-set :assets_prefix, 'pipeline_assets'
+# set :assets_prefix, 'pipeline_assets'
 
 # Default value for keep_releases is 5
 set :keep_releases, 5
