ed87cb9bb4d4807baaf701bd107b7a65460ecb48
nazrulcse <nazrulku07@gmail.com>
2017-10-30 19:57:44 +0600

    api controller added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  namespace :api, defaults: {format: :json} do
+    namespace :v1 do
+      resources :products, only: [:index]
+    end
+  end
+
   devise_for :users, controllers: {
                        sessions: 'users/sessions',
                        registrations: 'users/registrations',
