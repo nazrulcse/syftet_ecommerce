943733a6d202c50cf237e8b7df5cbb18967bda65
Nazrul <nazrulku07@gmail.com>
2017-11-05 14:16:31 +0600

    fixed blog create issue

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -3,4 +3,6 @@ class Blog < ApplicationRecord
   friendly_id :title, use: :slugged
   mount_uploader :cover_photo, Admin::BlogCoverPhotoUploader
   has_many :comments
+  
+  validates_presence_of :title, :body, :cover_photo
 end
