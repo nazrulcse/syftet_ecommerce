4daa421d9c660810aa9a085d32b0dc46260831f4
Nazrul <nazrulku07@gmail.com>
2017-11-13 12:19:25 +0600

    fixed category menu style

--- app/helpers/admin/taxons_helper.rb
+++ app/helpers/admin/taxons_helper.rb
@@ -9,11 +9,12 @@ module Admin
       html = "<div> #{node.name}(#{taxon_path(node)}) #{taxon_actions(node)} </div>"
       html += '<ul>'
       node.children.each do |child|
-        html += "<li> #{child.name}(#{taxon_path(child)}) #{taxon_actions(child)}"
         if child.children.any?
-          html += "<ol> #{draw_taxon_tree(child)} </ol>"
+          html += "#{draw_taxon_tree(child)}"
+        else
+          html += "<li> #{child.name}(#{taxon_path(child)}) #{taxon_actions(child)}"
+          html += '</li>'
         end
-        html += '</li>'
       end
       html + '</ul>'
     end
@@ -24,5 +25,25 @@ module Admin
       html += (link_to raw("<i class='icon icon-delete'></i>"), admin_taxonomy_taxon_path(taxon.taxonomy_id, taxon.id), confirm: 'Are you sure?', class: 'delete', method: :delete, remote: true)
       "<span class='taxon-actions'> #{html} </span>"
     end
+
+    def draw_category_tree(node, selected = '')
+      html = "<li> #{link_to node.name, categories_path(node.permalink), class: (selected == node.id ? 'active' : '')}"
+      html += "<span data-ref='top-#{node.id}' class='pull-right collapse-ref'> <i class='fa fa-plus'></i> </span>" if node.children.any?
+      if node.children.any?
+        html += "<ul id='top-#{node.id}'>"
+        node.children.each do |child|
+          if child.children.any?
+            html += "#{draw_category_tree(child, selected)}"
+          else
+            html += "<li> #{link_to child.name, categories_path(child.permalink), class: (selected == child.id ? 'active' : '')}"
+            html += "<span data-ref='top-#{child.id}' class='pull-right collapse-ref'> <i class='fa fa-plus'></i> </span>" if child.children.any?
+            html += '</li>'
+          end
+        end
+        html += '</ul>'
+      end
+      html + '</li>'
+    end
+
   end
 end
