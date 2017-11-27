43dd92033a5f7a59d9f582932edcfa28176540fb
Nazrul <nazrulku07@gmail.com>
2017-11-27 16:00:38 +0600

    add new email credential for general SMTP settings

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,13 +3,13 @@ class UserMailer < ApplicationMailer
 
   def welcome(user)
     @user = user
-    mail(to: user.email, from: 'sales@lienesbeauty.com', subject: 'Welcome To Lienesbeuty.com')
+    mail(to: user.email, from: 'shop@lienesbeauty.com', subject: 'Welcome To Lienesbeuty.com')
   end
 
   def reset_password_instructions(user, token, *args)
     @edit_password_reset_url = edit_user_password_url(:reset_password_token => token)
     @user = user
-    mail to: user.email, from: 'sales@lienesbeauty.com', subject: 'Lienesbeauty ' + I18n.t(:subject, :scope => [:devise, :mailer, :reset_password_instructions])
+    mail to: user.email, from: 'shop@lienesbeauty.com', subject: 'Lienesbeauty ' + I18n.t(:subject, :scope => [:devise, :mailer, :reset_password_instructions])
   end
 
 end
